5dc0c22547e5ce3eee2c2af71d6d2484d90d5ac7
parksil0 <41149744+parksil0@users.noreply.github.com>
2020-08-10 23:34:17 +0900

    Update ProductDAO.java

--- src/com/bc/model/dao/ProductDAO.java
+++ src/com/bc/model/dao/ProductDAO.java
@@ -16,7 +16,6 @@ import com.bc.vo.P_reviewVO;
 
 public class ProductDAO {
 
-	//�Խñ� ��ȸ
 	public static P_boardVO selectOne(String p_idx) {
 		SqlSession ss = DBService.getFactory().openSession();
 		P_boardVO vo = ss.selectOne("SHOP.pview", p_idx);
@@ -24,7 +23,6 @@ public class ProductDAO {
 		return vo;
 	}
 
-	//Q&A ��� ����
 	public static List<P_inquiryVO> showInquiry(String p_idx) {
 		SqlSession ss = DBService.getFactory().openSession();
 		List<P_inquiryVO> list = ss.selectList("SHOP.showInquiry", p_idx);
@@ -39,7 +37,6 @@ public class ProductDAO {
 		return list;
 	}
 	
-	//���� ��� ����
 	public static List<P_reviewVO> showReview(String p_idx) {
 		SqlSession ss = DBService.getFactory().openSession();
 		List<P_reviewVO> list = ss.selectList("SHOP.showReview", p_idx);
@@ -54,7 +51,6 @@ public class ProductDAO {
 		return list;
 	}
 	
-	//��ǰ �ɼ� ����
 	public static List<P_optionVO> showOption(String p_idx) {
 		SqlSession ss = DBService.getFactory().openSession();
 		List<P_optionVO> list = ss.selectList("SHOP.showOption", p_idx);
@@ -62,23 +58,17 @@ public class ProductDAO {
 		return list;
 	}
 	
-	//��ٱ��Ͽ� ���
-	public static int insertCart(Map<String, String> map) {
-		SqlSession ss = DBService.getFactory().openSession(true);
 		int result = ss.insert("SHOP.insertCart", map);
 		ss.close();
 		return result;
 	}
 	
-	//��ٱ��� ����Ʈ �ҷ�����
-	public static List<O_cartlistVO> showCartlist(String userid) {
 		SqlSession ss = DBService.getFactory().openSession();
 		List<O_cartlistVO> list = ss.selectList("SHOP.showCartlist", userid);
 		ss.close();
 		return list;
 	}
 	
-	//��ٱ��Ͽ� ������ ��ǰ�� �ִ��� Ȯ���ϱ�
 	public static int isDuplicate(Map<String, String> map) {
 		SqlSession ss = DBService.getFactory().openSession();
 		int result = ss.selectOne("SHOP.chkDuplicate", map);
